c0b4a5e54a406b5e4b709ac166dbc112ef384322
Özcan Çelik <ozcan@strajedi.com.tr>
2023-02-03 16:15:29 +0300

    Fixes.

--- wpgraphql-prev-next-random-post.php
+++ wpgraphql-prev-next-random-post.php
@@ -11,66 +11,64 @@ use WPGraphQL\Model\Post;
  * Version: 0.0.1
  */
 
-// Previous Post
-register_graphql_field('Post', 'previousPost', [
-    'type' => 'Post',
-    'description' => __(
-        'Previous Post'
-    ),
-    'resolve' => function (Post $post, array $args, AppContext $context) {
-        global $post;
-        $post = get_post($post->ID, OBJECT);
-        setup_postdata($post);
-        $prev = get_adjacent_post(true, '', true, 'language');
-        wp_reset_postdata();
-        if (!$prev) {
-            return null;
-        }
+add_action('graphql_register_types', 'register_post_object_fields');
+function register_post_object_fields()
+{
+    // Previous Post 
+    register_graphql_field('Post', 'previous', [
+        'type' => 'Post',
+        'description' => __(
+            'Previous Post'
+        ),
 
-        return DataSource::resolve_post_object($prev->ID, $context);
-    },
-]);
-// Next Post
-register_graphql_field('Post', 'nextPost', [
-    'type' => 'Post',
-    'description' => __(
-        'Next Post'
-    ),
-    'resolve' => function (Post $post, array $args, AppContext $context) {
-        global $post;
-        $post = get_post($post->ID, OBJECT);
-        setup_postdata($post);
-        $next = get_adjacent_post(true, '', false, 'language');
-        wp_reset_postdata();
-        if (!$next) {
-            return null;
-        }
+        'resolve' => function (Post $post, array $args, AppContext $context) {
+            global $post;
+            $post = get_post($post->ID, OBJECT);
+            setup_postdata($post);
+            $prev = get_adjacent_post(true, '', true);
+            wp_reset_postdata();
+            if (!$prev) {
+                return null;
+            }
 
-        return DataSource::resolve_post_object($next->ID, $context);
-    },
-]);
+            return DataSource::resolve_post_object($prev->ID, $context);
+        },
+    ]);
 
-// Random Post
+    // Next Post 
+    register_graphql_field('Post', 'next', [
+        'type' => 'Post',
+        'description' => __(
+            'Next Post'
+        ),
+        'resolve' => function (Post $post, array $args, AppContext $context) {
+            global $post;
+            $post = get_post($post->ID, OBJECT);
+            setup_postdata($post);
+            $next = get_adjacent_post(true, '', false);
+            wp_reset_postdata();
+            if (!$next) {
+                return null;
+            }
 
-register_graphql_field('RootQuery', 'randomPost', [
-    'type' => 'Post',
-    'description' => __('Random Post by Language'),
-    'args' => [
-        'language' => [
-            'type' => 'LanguageCodeEnum',
-            'description' => __('Language'),
-        ],
-    ],
-    'resolve' => function ($root, $args, $context, $info) {
-        $args = [
-            'post_type' => 'post',
-            'orderby' => 'rand',
-            'posts_per_page' => 1,
-            'lang' => $args['language'],
-        ];
-        $query = new \WP_Query($args);
-        $posts = $query->get_posts();
-        $post = $posts[0];
-        return DataSource::resolve_post_object($post->ID, $context);
-    },
-]);
+            return DataSource::resolve_post_object($next->ID, $context);
+        },
+    ]);
+    // Random Post 
+    register_graphql_field('rootQuery', 'randomPost', [
+        'type' => 'Post',
+        'description' => __('A random post', 'wpgraphql-random-post'),
+        'resolve' => function ($root, $args, $context, $info) {
+            $args = [
+                'post_type' => 'post',
+                'orderby' => 'rand',
+                'posts_per_page' => 1,
+                'lang' => $args['language'],
+            ];
+            $query = new \WP_Query($args);
+            $posts = $query->get_posts();
+            $post = $posts[0];
+            return DataSource::resolve_post_object($post->ID, $context);
+        },
+    ]);
+}
